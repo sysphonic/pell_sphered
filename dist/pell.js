19f5ea1b5e437f5acfc7a2e2f8f271880f59ca45
MORITA Shintaro <shin@sysphonic.com>
2019-07-17 07:46:02 +0900

    Bug-fix: oninput() called recursively on Chrome. Signed-off-by: MORITA Shintaro <shin@sysphonic.com>

--- dist/pell.js
+++ dist/pell.js
@@ -2,15 +2,15 @@ var NESSIE_RELATIVE_URL_ROOT = "./nessie";  // "/nessie" is recommended.
 var NESSIE_OFFSET_FROM_THIS_FILE = "../";   // Unless so, and if necessary
 
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	typeof exports === "object" && typeof module !== "undefined" ? factory(exports) :
+	typeof define === "function" && define.amd ? define(["exports"], factory) :
 	(factory((global.pell = {})));
-}(this, (function (exports) { 'use strict';
+}(this, (function (exports) { "use strict";
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var defaultParagraphSeparatorString = 'defaultParagraphSeparator';
-var formatBlock = 'formatBlock';
+var defaultParagraphSeparatorString = "defaultParagraphSeparator";
+var formatBlock = "formatBlock";
 var addEventListener = function addEventListener(parent, type, listener) {
   return parent.addEventListener(type, listener);
 };
@@ -27,113 +27,120 @@ var queryCommandValue = function queryCommandValue(command) {
   return document.queryCommandValue(command);
 };
 
-var exec = function exec(command) {
+var exec = function exec(command, skipOnInput) {
   var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-  var ret = document.execCommand(command, false, value);
   var content = _settings.element.content;
-  content.oninput({target:content});  // for Edge
+
+  var func = content.oninput;
+  content.oninput = function() {};
+  var ret = document.execCommand(command, false, value);
+  content.oninput = func;
+
+  if (!skipOnInput) {
+    content.oninput({target:content});  // for Edge
+  }
   return ret;
 };
 
 var defaultActions = {
   bold: {
-    icon: '<b>B</b>',
+    icon: "<b>B</b>",
     title: t("action.bold"),
     state: function state() {
-      return queryCommandState('bold');
+      return queryCommandState("bold");
     },
     result: function result() {
-      return exec('bold');
+      return exec("bold");
     }
   },
   italic: {
-    icon: '<i>I</i>',
+    icon: "<i>I</i>",
     title: t("action.italic"),
     state: function state() {
-      return queryCommandState('italic');
+      return queryCommandState("italic");
     },
     result: function result() {
-      return exec('italic');
+      return exec("italic");
     }
   },
   underline: {
-    icon: '<u>U</u>',
+    icon: "<u>U</u>",
     title: t("action.underline"),
     state: function state() {
-      return queryCommandState('underline');
+      return queryCommandState("underline");
     },
     result: function result() {
-      return exec('underline');
+      return exec("underline");
     }
   },
   strikethrough: {
-    icon: '<strike>S</strike>',
+    icon: "<strike>S</strike>",
     title: t("action.strike_through"),
     state: function state() {
-      return queryCommandState('strikeThrough');
+      return queryCommandState("strikeThrough");
     },
     result: function result() {
-      return exec('strikeThrough');
+      return exec("strikeThrough");
     }
   },
   heading1: {
-    icon: '<b>H<sub>1</sub></b>',
+    icon: "<b>H<sub>1</sub></b>",
     title: t("action.heading1"),
     result: function result() {
-      return exec(formatBlock, '<h1>');
+      return exec(formatBlock, "<h1>");
     }
   },
   heading2: {
-    icon: '<b>H<sub>2</sub></b>',
+    icon: "<b>H<sub>2</sub></b>",
     title: t("action.heading2"),
     result: function result() {
-      return exec(formatBlock, '<h2>');
+      return exec(formatBlock, "<h2>");
     }
   },
   paragraph: {
-    icon: '&#182;',
+    icon: "&#182;",
     title: t("action.paragraph"),
     result: function result() {
-      return exec(formatBlock, '<p>');
+      return exec(formatBlock, "<p>");
     }
   },
   quote: {
-    icon: '&#8220; &#8221;',
+    icon: "&#8220; &#8221;",
     title: t("action.quote"),
     result: function result() {
-      return exec(formatBlock, '<blockquote>');
+      return exec(formatBlock, "<blockquote>");
     }
   },
   olist: {
-    icon: '&#35;',
+    icon: "&#35;",
     title: t("action.ordered_list"),
     result: function result() {
-      return exec('insertOrderedList');
+      return exec("insertOrderedList");
     }
   },
   ulist: {
-    icon: '&#8226;',
+    icon: "&#8226;",
     title: t("action.unordered_list"),
     result: function result() {
-      return exec('insertUnorderedList');
+      return exec("insertUnorderedList");
     }
   },
   code: {
-    icon: '&lt;/&gt;',
+    icon: "&lt;/&gt;",
     title: t("action.code"),
     result: function result() {
-      return exec(formatBlock, '<pre>');
+      return exec(formatBlock, "<pre>");
     }
   },
   line: {
-    icon: '&#8213;',
+    icon: "&#8213;",
     title: t("action.horz_line"),
     result: function result() {
-      return exec('insertHorizontalRule');
+      return exec("insertHorizontalRule");
     }
   },
   link: {
-    icon: '&#128279;',
+    icon: "&#128279;",
     title: t("action.link"),
     result: function result() {
       var thetisBox = new ThetisBox;
@@ -141,7 +148,7 @@ var defaultActions = {
     }
   },
   image: {
-    icon: '&#128247;',
+    icon: "&#128247;",
     title: t("action.image"),
     result: function result() {
       var thetisBox = new ThetisBox;
@@ -149,14 +156,14 @@ var defaultActions = {
     }
   },
   forecolor: {
-    icon: '<span id="pell_forecolor"><img src="'+NESSIE_RELATIVE_URL_ROOT+'/img/icons/text_color.png" /><input type="hidden" id="pell_forecolor_val" /></span>',
+    icon: "<span id=\"pell_forecolor\"><img src=\""+NESSIE_RELATIVE_URL_ROOT+"/img/icons/text_color.png\" /><input type=\"hidden\" id=\"pell_forecolor_val\" /></span>",
     title: t("action.forecolor"),
     result: function result() {
       showPalette("pell_forecolor");
     }
   },
   bgcolor: {
-    icon: '<span id="pell_bgcolor"><img src="'+NESSIE_RELATIVE_URL_ROOT+'/img/icons/bgcolor.png" /><input type="hidden" id="pell_bgcolor_val" /></span>',
+    icon: "<span id=\"pell_bgcolor\"><img src=\""+NESSIE_RELATIVE_URL_ROOT+"/img/icons/bgcolor.png\" /><input type=\"hidden\" id=\"pell_bgcolor_val\" /></span>",
     title: t("action.bgcolor"),
     result: function result() {
       showPalette("pell_bgcolor");
@@ -165,17 +172,18 @@ var defaultActions = {
 };
 
 var defaultClasses = {
-  actionbar: 'pell-actionbar',
-  button: 'pell-button',
-  content: 'pell-content',
-  selected: 'pell-button-selected'
+  actionbar: "pell-actionbar",
+  button: "pell-button",
+  content: "pell-content",
+  selected: "pell-button-selected",
+  copyright: "pell-copyright"
 };
 
 var init = function init(settings) {
   _settings = settings;
 
   var actions = settings.actions ? settings.actions.map(function (action) {
-    if (typeof action === 'string') return defaultActions[action];else if (defaultActions[action.name]) return _extends({}, defaultActions[action.name], action);
+    if (typeof action === "string") return defaultActions[action];else if (defaultActions[action.name]) return _extends({}, defaultActions[action.name], action);
     return action;
   }) : Object.keys(defaultActions).map(function (action) {
     return defaultActions[action];
@@ -183,13 +191,13 @@ var init = function init(settings) {
 
   var classes = _extends({}, defaultClasses, settings.classes);
 
-  var defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || 'div';
+  var defaultParagraphSeparator = settings[defaultParagraphSeparatorString] || "div";
 
-  var actionbar = createElement('div');
+  var actionbar = createElement("div");
   actionbar.className = classes.actionbar;
   appendChild(settings.element, actionbar);
 
-  var content = settings.element.content = createElement('div');
+  var content = settings.element.content = createElement("div");
   content.contentEditable = true;
   content.className = classes.content;
   content.oninput = function (_ref) {
@@ -198,25 +206,29 @@ var init = function init(settings) {
     }
     var firstChild = _ref.target.firstChild;
 
-    if (firstChild && firstChild.nodeType === 3) exec(formatBlock, '<' + defaultParagraphSeparator + '>');else if (content.innerHTML === '<br>') content.innerHTML = '';
+    if (firstChild && firstChild.nodeType === 3) {
+      exec(formatBlock, "<" + defaultParagraphSeparator + ">", true);
+    } else if (content.innerHTML === "<br>") {
+      content.innerHTML = "";
+    }
     settings.onChange(content.innerHTML);
     _pellLastHtml = content.innerHTML;
   };
   content.onkeydown = function (event) {
-    if (event.key === 'Enter' && queryCommandValue(formatBlock) === 'blockquote') {
+    if (event.key === "Enter" && queryCommandValue(formatBlock) === "blockquote") {
       setTimeout(function () {
-        return exec(formatBlock, '<' + defaultParagraphSeparator + '>');
+        return exec(formatBlock, "<" + defaultParagraphSeparator + ">");
       }, 0);
     }
   };
   appendChild(settings.element, content);
 
   actions.forEach(function (action) {
-    var button = createElement('button');
+    var button = createElement("button");
     button.className = classes.button;
     button.innerHTML = action.icon;
     button.title = action.title;
-    button.setAttribute('type', 'button');
+    button.setAttribute("type", "button");
     button.onclick = function () {
       setSelection(content);
       return action.result() && content.focus();
@@ -224,17 +236,25 @@ var init = function init(settings) {
 
     if (action.state) {
       var handler = function handler() {
-        return button.classList[action.state() ? 'add' : 'remove'](classes.selected);
+        return button.classList[action.state() ? "add" : "remove"](classes.selected);
       };
-      addEventListener(content, 'keyup', handler);
-      addEventListener(content, 'mouseup', handler);
-      addEventListener(button, 'click', handler);
+      addEventListener(content, "keyup", handler);
+      addEventListener(content, "mouseup", handler);
+      addEventListener(button, "click", handler);
     }
 
     appendChild(actionbar, button);
   });
 
-  if (settings.styleWithCSS) exec('styleWithCSS');
+  var copyright = createElement("a");
+  copyright.className = classes.copyright;
+  copyright.target = "pell";
+  copyright.href = "https://github.com/sysphonic/pell_sphered";
+  copyright.title = "pell: Copyright (c) Jared Reich";
+  copyright.innerHTML = "(c)"
+  appendChild(actionbar, copyright);
+
+  if (settings.styleWithCSS) exec("styleWithCSS");
   exec(defaultParagraphSeparatorString, defaultParagraphSeparator);
 
   return settings.element;
@@ -309,9 +329,9 @@ var pell = {
 exports.exec = exec;
 exports.init = init;
 exports.act = act;
-exports['default'] = pell;
+exports["default"] = pell;
 
-Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 
 })));
 
